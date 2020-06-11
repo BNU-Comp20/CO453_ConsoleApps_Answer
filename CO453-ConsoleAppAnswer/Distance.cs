315ee4289537a8976d43aac76ec128b01ec6b40b
DerekPeacock <derek.peacock@yahoo.com>
2020-06-11 11:41:34 +0100

    Add project files.

---
+++ CO453-ConsoleAppAnswer/Distance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CO453_ConsoleAppAnswer
+{
+    /// <summary>
+    /// This class offers methods for prompting the use
+    /// to enter the number of miles, and then converting
+    /// that into the equivalent number feet
+    /// </summary>
+    /// <author>Derek Peacock</author>
+    public class Distance
+    {
+        public const int MILES_TO_FEET = 1760 * 3;
+
+        private double miles, feet;
+
+        public void ConvertToFeet()
+        {
+            Console.WriteLine("Convert Miles to Feet");
+
+            inputMiles();
+
+            feet = miles * MILES_TO_FEET;
+
+            outputFeet();
+        }
+
+        private void outputFeet()
+        {
+            Console.WriteLine(miles + " miles is " + feet + " feet!");
+        }
+
+        private void inputMiles()
+        {
+            Console.Write("Enter the number of miles >");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+    }
+}
---
+++ CO453-ConsoleAppAnswer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CO453_ConsoleAppAnswer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Derek's Console Apps 2020");
+            Distance converter = new Distance();
+            converter.ConvertToFeet();
+        }
+    }
+}
